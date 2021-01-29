9030c4a5206a9df375412c277609e73636b7be5b
Tyler Hawkins <thawkin3@byu.net>
2021-01-29 10:54:12 -0700

    feat: adds a public directory for html/css/js

--- src/server.js
+++ src/server.js
@@ -13,6 +13,6 @@ const server = new ApolloServer({
   })
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 });
